dea93bdb628d2b1d1ddc5a7b7d578e05dd5d7d86
Konduru Pranav Suhas Reddy <pranavsuhasreddykonduru@gmail.com>
2024-09-29 16:12:30 +0530

    reset old server code

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const http = require('http');
-const path = require('path');
+const path=require('path');
 const { Server } = require('socket.io');
 const cors = require('cors');
 
@@ -10,131 +10,78 @@ app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: '*',
+    origin: '*',  
     methods: ['GET', 'POST'],
-  },
+  }
 });
 
 app.use(express.static('build'));
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+app.use((req,res,next)=>
+{
+  res.sendFile(path.join(__dirname, 'build','index.html'));
+})
 
-const rooms = {}; 
+const rooms = {};
 
 io.on('connection', (socket) => {
   console.log('New client connected:', socket.id);
 
   socket.on('joinRoom', ({ roomId, username }) => {
     if (!rooms[roomId]) {
-      rooms[roomId] = {
-        users: [],
-        code: '', 
-        language: 'c', 
-        input: '', 
-        output: '', 
-      };
+      rooms[roomId] = [];
     }
-  
-    rooms[roomId].users.push({ id: socket.id, username });
-  
+
+    rooms[roomId].push({ id: socket.id, username });
     socket.join(roomId);
-  
-    socket.emit('syncState', {
-      code: rooms[roomId].code,
-      language: rooms[roomId].language,
-      input: rooms[roomId].input,
-      output: rooms[roomId].output,
-    });
-  
+
     socket.to(roomId).emit('userJoined', { username });
-  
-    io.to(roomId).emit('roomUsers', rooms[roomId].users);
-  
+    
     console.log(`${username} joined room ${roomId}`);
+    io.to(roomId).emit('roomUsers', rooms[roomId]); 
   });
-  
 
   socket.on('codeChange', ({ roomId, code }) => {
-    if (rooms[roomId]) {
-      rooms[roomId].code = code; 
-      socket.to(roomId).emit('codeUpdate', code); 
-      io.to(roomId).emit('syncState', { 
-        code: rooms[roomId].code,
-        language: rooms[roomId].language,
-        input: rooms[roomId].input,
-        output: rooms[roomId].output,
-      });
-    }
+    socket.to(roomId).emit('codeUpdate', code);
   });
-
+  
   socket.on('languageChange', ({ roomId, language }) => {
-    if (!rooms[roomId]) return;
-    rooms[roomId].language = language;
     socket.to(roomId).emit('languageUpdate', language);
   });
 
-  socket.on('inputChange', ({ roomId, input }) => {
-    if (rooms[roomId]) {
-      rooms[roomId].input = input; 
-      socket.to(roomId).emit('inputUpdate', input); 
-      io.to(roomId).emit('syncState', { 
-        code: rooms[roomId].code,
-        language: rooms[roomId].language,
-        input: rooms[roomId].input,
-        output: rooms[roomId].output,
-      });
-    }
-  });
-  
-  socket.on('outputChange', ({ roomId, output }) => {
-    if (rooms[roomId]) {
-      rooms[roomId].output = output; 
-      socket.to(roomId).emit('outputUpdate', output); 
-      io.to(roomId).emit('syncState', { 
-        code: rooms[roomId].code,
-        language: rooms[roomId].language,
-        input: rooms[roomId].input,
-        output: rooms[roomId].output,
-      });
-    }
-  });
+socket.on('inputChange', ({ roomId, input }) => {
+  socket.to(roomId).emit('inputUpdate', input); 
+});
 
-  socket.on('leaveRoom', ({ roomId }) => {
-    if (!rooms[roomId]) return;
-    const userIndex = rooms[roomId].users.findIndex((user) => user.id === socket.id);
-    if (userIndex !== -1) {
-      rooms[roomId].users.splice(userIndex, 1);
-      io.to(roomId).emit('roomUsers', rooms[roomId].users);
-    }
+socket.on('outputChange', ({ roomId, output }) => {
+  socket.to(roomId).emit('outputUpdate', output); 
+});
 
-    if (rooms[roomId].users.length === 0) delete rooms[roomId];
-
-    socket.leave(roomId);
-    console.log(`Socket ${socket.id} left room ${roomId}`);
-  });
 
   socket.on('disconnect', () => {
     let roomId;
     let username;
 
     for (let room in rooms) {
-      const userIndex = rooms[room].users.findIndex((user) => user.id === socket.id);
+      const userIndex = rooms[room].findIndex(user => user.id === socket.id);
       if (userIndex !== -1) {
-        username = rooms[room].users[userIndex].username;
+        username = rooms[room][userIndex].username;
         roomId = room;
-        rooms[room].users.splice(userIndex, 1);
+        rooms[room].splice(userIndex, 1);
         break;
       }
     }
 
     if (roomId) {
       io.to(roomId).emit('userLeft', { username });
-      io.to(roomId).emit('roomUsers', rooms[roomId].users);
+      io.to(roomId).emit('roomUsers', rooms[roomId]);
 
-      if (rooms[roomId].users.length === 0) delete rooms[roomId];
       console.log(`${username} left room ${roomId}`);
     }
+  });
+
+  socket.on('leaveRoom', ({ roomId }) => {
+    socket.leave(roomId);
+    console.log(`Socket ${socket.id} left room ${roomId}`);
   });
 
   socket.on('error', (err) => {
